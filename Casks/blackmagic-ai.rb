5ea134e2c2f8a43d78675392c1c1086902e21e08
Lynn Mikami <192083063+furudo-erika@users.noreply.github.com>
2026-04-20 14:13:46 +0800

    blackmagic-ai 0.2.7

--- Casks/blackmagic-ai.rb
+++ Casks/blackmagic-ai.rb
@@ -1,14 +1,14 @@
 cask "blackmagic-ai" do
-  version "0.2.6"
+  version "0.2.7"
 
   on_arm do
-    sha256 "8086ceeb9ce49e8242a7bb0146c3d80d96f89e51f9eb78624e65f5e58764cb20"
+    sha256 "7aa2d781899c0fa216cd6fbd08435eaee7b09f60ae26d8647b6389ff950aa2a2"
     url "https://pub-d259d1d2737843cb8bcb2b1ff98fc9c6.r2.dev/blackmagic-desktop/BlackMagic%20AI-#{version}-arm64.dmg",
         verified: "pub-d259d1d2737843cb8bcb2b1ff98fc9c6.r2.dev/blackmagic-desktop/"
   end
 
   on_intel do
-    sha256 "b5ffb41d8d7629ed332f7c8f65143e3e51918f64af9a7f9d1654821bfa839820"
+    sha256 "f8b062b7fb6382078c96e34272c0c25cd2ac77fed81963a1c4eee96c57de7479"
     url "https://pub-d259d1d2737843cb8bcb2b1ff98fc9c6.r2.dev/blackmagic-desktop/BlackMagic%20AI-#{version}.dmg",
         verified: "pub-d259d1d2737843cb8bcb2b1ff98fc9c6.r2.dev/blackmagic-desktop/"
   end
@@ -18,6 +18,12 @@ cask "blackmagic-ai" do
   homepage "https://github.com/furudo-erika/blackmagic-desktop"
 
   app "BlackMagic AI.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/BlackMagic AI.app"],
+                   sudo: false
+  end
 
   zap trash: [
     "~/Library/Application Support/BlackMagic AI",
